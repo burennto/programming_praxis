130b65eb3a1c5b33090e7ac0f29ee13a0a52cbc8
Brent Chuang <burennto@gmail.com>
2011-09-20 21:22:15 +1000

    Added Exercise 1: RPN Calculator

---
+++ 001_rpn_calculator/rpn_calculator.rb
@@ -0,0 +1,18 @@
+# Programming Praxis
+# Exercise 1: RPN Calculator
+# http://programmingpraxis.com/2009/02/19/rpn-calculator/
+#
+# Implement an RPN calculator that takes an expression like 19 2.14 + 4.5 2 4.3 / - *
+# which is usually expressed as (19 + 2.14) * (4.5 - 2 / 4.3) and responds with 85.2974.
+# The program should read expressions from standard input and print the top of the stack to
+# standard output when a newline is encountered. The program should retain the state of the
+# operand stack between expressions.
+#
+# Author: Brent Chuang
+# Date:   2/1/2011
+
+class RPNCalculator
+
+
+  
+end

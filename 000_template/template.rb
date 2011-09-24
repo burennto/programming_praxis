dd88af92dcffe5960ebd03c2537e4eebdde4be8a
Brent Chuang <burennto@gmail.com>
2011-09-24 23:17:13 +1000

    Updated template

---
+++ 000_template/template.rb
@@ -0,0 +1,29 @@
+# Programming Praxis
+# Exercise ###:
+# http://programmingpraxis.com/.....
+#
+# Author: Brent Chuang (burennto@gmail.com)
+# Date:   
+# 
+
+module Ex
+  class << self
+    
+    def
+
+    end
+    
+  end
+end
+
+
+
+require 'test/unit'
+
+class TestEx < Test::Unit::TestCase
+  
+  def test_
+    
+  end
+  
+end
